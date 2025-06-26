6f2fdccbdf1510394546482754e4c705eb9e66ca
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2025-06-27 01:10:38 +0500

    new commit

--- backend_app/routes/index.js
+++ backend_app/routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const userModel = require("./users");
+
+router.get('/',(req,res)=>{
+  req.session.banned = false;
+  res.cookie("age",12)
+  res.render("index");
+
+})
+
+router.get("/readcookie",(req,res)=>{
+ console.log(req.cookies.age)
+ res.send("check cookie");
+})
+
+router.get("/checkban",(req,res)=>{
+  if(req.session.banned===true){
+    res.send("You are banned");
+  }
+  else{
+    res.send("You are not banned");
+  }
+})
+
+router.get("/create",async (req,res)=>{
+   const createduser = await userModel.create({
+    username: "Sarim_123",
+    age:19,
+    name:"Muhammad Sarim Umer"
+   })
+   res.send(createduser);
+})
+
+router.get("/read",async (req,res)=>{
+  let Allusers = await userModel.findOne({
+    username: "Sarim_123"
+  });
+  res.send(Allusers);
+})
+
+router.get("/delete",async(req,res)=>{
+  let deleteduser = await userModel.findOneAndDelete({
+    username: "Sarim_123"
+  });
+  res.send(deleteduser);
+})
 
 module.exports = router;
