f1be1da3cf512c1e75bbe1beb3edcf4a0935ec03
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2025-07-02 22:48:00 +0500

    New Commit

---
+++ sm-backend/hello-world.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+setTimeout(()=>{
+    console.log("Hello World after 3 seconds");
+},3000)
+
+console.log("last line of program");
---
+++ sm-backend/node-module-exports/index.js
@@ -0,0 +1,9 @@
+const calculator = require("./firstmodule");
+
+try {
+    console.log(calculator.divide(10,10));
+    
+} catch (error) {
+    console.log(error);
+    
+}
